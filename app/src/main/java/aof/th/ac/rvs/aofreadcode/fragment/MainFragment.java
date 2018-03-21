a06b6211ff56d3e123e8163e1bae6f03c448081e
masterAOF <abc@cdf.com>
2018-03-21 14:49:06 +0700

    Create Toolbar Success

--- app/src/main/java/aof/th/ac/rvs/aofreadcode/fragment/MainFragment.java
+++ app/src/main/java/aof/th/ac/rvs/aofreadcode/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import aof.th.ac.rvs.aofreadcode.R;
 
@@ -14,6 +15,28 @@ import aof.th.ac.rvs.aofreadcode.R;
  */
 
 public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // Register Controller
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+
+
+    }
 
     @Nullable
     @Override
--- app/src/main/java/aof/th/ac/rvs/aofreadcode/fragment/RegisterFragment.java
+++ app/src/main/java/aof/th/ac/rvs/aofreadcode/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package aof.th.ac.rvs.aofreadcode.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import aof.th.ac.rvs.aofreadcode.MainActivity;
 import aof.th.ac.rvs.aofreadcode.R;
 
 /**
@@ -14,6 +16,29 @@ import aof.th.ac.rvs.aofreadcode.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+     //   Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+     //    Setup Title
+           ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+           // Show Navigator Icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    } // Main Method
 
     @Nullable
     @Override
