ed60565949bc693fabc90959bb476203fd59db95
masterAOF <abc@cdf.com>
2018-03-22 15:52:31 +0700

    Authen OK

--- app/src/main/java/aof/th/ac/rvs/aofreadcode/utility/MyConstant.java
+++ app/src/main/java/aof/th/ac/rvs/aofreadcode/utility/MyConstant.java
@@ -7,8 +7,20 @@ package aof.th.ac.rvs.aofreadcode.utility;
 public class MyConstant {
 
     //    About URL
+    private String urlGetAllUserString = "http://androidthai.in.th/mar/getAllUser.php";
     private String urlPostUserString = "http://androidthai.in.th/mar/postUser.php";
 
+    // About Array
+    private String[] loginString = new String[]{"id", "Name", "User", "Password"};
+
+
+    public String[] getLoginString() {
+        return loginString;
+    }
+
+    public String getUrlGetAllUserString() {
+        return urlGetAllUserString;
+    }
 
     public String getUrlPostUserString() {
         return urlPostUserString;
